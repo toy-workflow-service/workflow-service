b7e39f56a6a1abe4a612e122c5cfbfc0d33ce8d3
choisumin <tnals634@naver.com>
2023-08-17 23:38:07 +0900

    fix: board-column, board contorller 수정

--- src/board-columns/board-columns.controller.ts
+++ src/board-columns/board-columns.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Query, Res } from '@nestjs/common';
 import { BoardColumnsService } from './board-columns.service';
 import { CreateBoardColumnDto } from '../_common/dtos/create-board-column.dto';
 import { UpdateBoardColumnNameDto } from '../_common/dtos/update-board-column-name.dto';
 import { UpdateBoardColumnSequenceDto } from '../_common/dtos/update-board-column-sequence.dto';
+import { Response } from 'express';
 
 @Controller('board-columns')
 export class BoardColumnsController {
@@ -10,14 +11,16 @@ export class BoardColumnsController {
 
   //보드 칼럼 조회
   @Get('/columns')
-  async GetBoardColumns(@Query('boardId') boardId: number) {
-    return await this.boardColumnsService.GetBoardColumns(boardId);
+  async GetBoardColumns(@Query('boardId') boardId: number, @Res() res: Response) {
+    const columns = await this.boardColumnsService.GetBoardColumns(boardId);
+    return res.status(HttpStatus.OK).json(columns);
   }
 
   //보드 칼럼 생성
   @Post('/columns')
-  async PostBoardColumn(@Query('boardId') boardId: number, @Body() data: CreateBoardColumnDto) {
+  async PostBoardColumn(@Query('boardId') boardId: number, @Body() data: CreateBoardColumnDto, @Res() res: Response) {
     await this.boardColumnsService.PostBoardColumn(boardId, data.name, data.sequence);
+    return res.status(HttpStatus.CREATED).json({ message: '칼럼을 생성하였습니다.' });
   }
 
   //보드 칼럼 이름 수정
@@ -26,8 +29,10 @@ export class BoardColumnsController {
     @Query('boardId') boardId: number,
     @Param('columnId') columnId: number,
     @Body() data: UpdateBoardColumnNameDto,
+    @Res() res: Response,
   ) {
     await this.boardColumnsService.UpdateBoardColumnName(boardId, columnId, data.name);
+    return res.status(HttpStatus.OK).json({ message: '칼럼 이름을 수정하였습니다.' });
   }
 
   //보드 칼럼 시퀀스 수정
@@ -36,13 +41,20 @@ export class BoardColumnsController {
     @Query('boardId') boardId: number,
     @Param('columnId') columnId: number,
     @Body() data: UpdateBoardColumnSequenceDto,
+    @Res() res: Response,
   ) {
     await this.boardColumnsService.UpdateBoardColumnSequence(boardId, columnId, data.sequence);
+    return res.status(HttpStatus.OK).json({ message: '칼럼 순서를 수정하였습니다.' });
   }
 
   //보드 컬럼 삭제
   @Delete('/columns/:columnId')
-  async DeleteBoardColumn(@Query('boardId') boardId: number, @Param('columnId') columnId: number) {
+  async DeleteBoardColumn(
+    @Query('boardId') boardId: number,
+    @Param('columnId') columnId: number,
+    @Res() res: Response,
+  ) {
     await this.boardColumnsService.DeleteBoardColumn(boardId, columnId);
+    return res.status(HttpStatus.OK).json({ message: '칼럼을 삭제하였습니다.' });
   }
 }
--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Query, Res } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { CreateBoardDto } from './create-board.dto';
-import { UpdateBoardDto } from './update-board.dto';
+import { CreateBoardDto } from '../_common/dtos/create-board.dto';
+import { UpdateBoardDto } from '../_common/dtos/update-board.dto';
+import { Response } from 'express';
 
 @Controller('boards')
 export class BoardsController {
@@ -9,20 +10,23 @@ export class BoardsController {
 
   //보드 조회
   @Get('/boards')
-  async GetBoards(@Query('workspaceId') workspaceId: number) {
-    return await this.boardsService.GetBoards(workspaceId);
+  async GetBoards(@Query('workspaceId') workspaceId: number, @Res() res: Response) {
+    const boards = await this.boardsService.GetBoards(workspaceId);
+    return res.status(HttpStatus.OK).json({ boards });
   }
 
   //보드 상세 조회
   @Get('/boards/:boardId')
-  async GetBoardById(@Query('workspaceId') workspaceId: number, @Param('boardId') id: number) {
-    return await this.boardsService.GetBoardById(workspaceId, id);
+  async GetBoardById(@Query('workspaceId') workspaceId: number, @Param('boardId') id: number, @Res() res: Response) {
+    const board = await this.boardsService.GetBoardById(workspaceId, id);
+    return res.status(HttpStatus.OK).json({ board });
   }
 
   //보드 생성
   @Post('/boards')
-  async CreateBoard(@Query('workspaceId') workspaceId: number, @Body() data: CreateBoardDto) {
-    return await this.boardsService.CreateBoard(workspaceId, data.name, data.description);
+  async CreateBoard(@Query('workspaceId') workspaceId: number, @Body() data: CreateBoardDto, @Res() res: Response) {
+    await this.boardsService.CreateBoard(workspaceId, data.name, data.description);
+    return res.status(HttpStatus.CREATED).json({ message: '보드를 생성하였습니다.' });
   }
 
   //보드 수정
@@ -31,13 +35,16 @@ export class BoardsController {
     @Query('workspaceId') workspaceId: number,
     @Param('boardId') id: number,
     @Body() data: UpdateBoardDto,
+    @Res() res: Response,
   ) {
-    return await this.boardsService.UpdateBoard(workspaceId, id, data.name, data.description);
+    await this.boardsService.UpdateBoard(workspaceId, id, data.name, data.description);
+    return res.status(HttpStatus.OK).json({ message: '보드를 수정하였습니다.' });
   }
 
   //보드 삭제
   @Delete('/boards/:boardId')
-  async DeleteBoard(@Query('workspaceId') workspaceId: number, @Param('boardId') id: number) {
-    return await this.boardsService.DeleteBoard(workspaceId, id);
+  async DeleteBoard(@Query('workspaceId') workspaceId: number, @Param('boardId') id: number, @Res() res: Response) {
+    await this.boardsService.DeleteBoard(workspaceId, id);
+    return res.status(HttpStatus.OK).json({ message: '보드를 삭제하였습니다.' });
   }
 }
