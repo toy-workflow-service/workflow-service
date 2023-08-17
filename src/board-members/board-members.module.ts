f28247b25d947826cca358cb9b436c8dda494959
Sung min Park <somemen1234@naver.com>
2023-08-17 22:13:53 +0900

    conflict resolved

--- src/board-members/board-members.module.ts
+++ src/board-members/board-members.module.ts
@@ -3,9 +3,11 @@ import { BoardMembersService } from './board-members.service';
 import { BoardMembersController } from './board-members.controller';
 import { Board_Member } from 'src/_common/entities/board-member.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/_common/entities/user.entitiy';
+import { Board } from 'src/_common/entities/board.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Board_Member])],
+  imports: [TypeOrmModule.forFeature([Board_Member, User, Board])],
   exports: [TypeOrmModule],
   controllers: [BoardMembersController],
   providers: [BoardMembersService],
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -3,9 +3,10 @@ import { BoardsService } from './boards.service';
 import { BoardsController } from './boards.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Board } from 'src/_common/entities/board.entity';
+import { Workspace } from 'src/_common/entities/workspace.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Board])],
+  imports: [TypeOrmModule.forFeature([Board, Workspace])],
   exports: [TypeOrmModule],
   controllers: [BoardsController],
   providers: [BoardsService],
