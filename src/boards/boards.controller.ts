da1c67643741a6c86d512895317f1e0255971428
choisumin <tnals634@naver.com>
2023-08-17 20:14:15 +0900

    fix: UseGuards 제거

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,8 +1,7 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './create-board.dto';
 import { UpdateBoardDto } from './update-board.dto';
-import { AuthGuard } from 'src/_common/security/auth.guard';
 
 @Controller('boards')
 export class BoardsController {
@@ -10,28 +9,24 @@ export class BoardsController {
 
   //보드 조회
   @Get('/boards/:workspaceId')
-  @UseGuards(AuthGuard)
   async GetBoards(@Param('workspaceId') workspaceId: number) {
     return await this.boardsService.GetBoards(workspaceId);
   }
 
   //보드 상세 조회
   @Get('/boards/:workspaceId/:boardId')
-  @UseGuards(AuthGuard)
   async GetBoardById(@Param('workspaceId') workspaceId: number, @Param('boardId') id: number) {
     return await this.boardsService.GetBoardById(workspaceId, id);
   }
 
   //보드 생성
   @Post('/boards/:workspaceId')
-  @UseGuards(AuthGuard)
   async CreateBoard(@Param('workspaceId') workspaceId: number, @Body() data: CreateBoardDto) {
     return await this.boardsService.CreateBoard(workspaceId, data.name, data.description);
   }
 
   //보드 수정
   @Put('/boards/:workspaceId/:boardId')
-  @UseGuards(AuthGuard)
   async UpdateBoard(
     @Param('workspaceId') workspaceId: number,
     @Param('boardId') id: number,
@@ -42,7 +37,6 @@ export class BoardsController {
 
   //보드 삭제
   @Delete('/boards/:workspaceId/:boardId')
-  @UseGuards(AuthGuard)
   async DeleteBoard(@Param('workspaceId') workspaceId: number, @Param('boardId') id: number) {
     return await this.boardsService.DeleteBoard(workspaceId, id);
   }
