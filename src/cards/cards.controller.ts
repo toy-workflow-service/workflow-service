bf3d84c88eabbda668144003c75bd131d1ef2a2e
choisumin <tnals634@naver.com>
2023-08-25 09:03:11 +0900

    feat: card sequence 관련 service, controller 추가

--- src/cards/cards.controller.ts
+++ src/cards/cards.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Param, Post, Patch, Delete, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Patch, Delete, Query, UseGuards, Put } from '@nestjs/common';
 import { CardsService } from './cards.service';
 import { CreateCardDto } from '../_common/dtos/create-card.dto';
 import { UpdateCardDto } from '../_common/dtos/update-card.dto';
 import { AuthGuard } from 'src/_common/security/auth.guard';
+import { UpdateCardSequenceDto } from 'src/_common/dtos/update-card-sequence.dto';
 
 @Controller('cards')
 export class CardsController {
@@ -43,14 +44,7 @@ export class CardsController {
     @Param('cardId') id: number,
     @Body() data: UpdateCardDto
   ) {
-    return await this.cardsService.UpdateCard(
-      board_column_Id,
-      id,
-      data.name,
-      data.content,
-      data.file_url,
-      data.sequence
-    );
+    return await this.cardsService.UpdateCard(board_column_Id, id, data.name, data.content, data.file_url);
   }
 
   //카드 삭제
@@ -58,5 +52,16 @@ export class CardsController {
   @UseGuards(AuthGuard)
   async DeleteCard(@Query('board_column_Id') columnId: number, @Param('cardId') id: number) {
     return await this.cardsService.DeleteCard(columnId, id);
+  }
+
+  //카드 시퀀스 수정
+  @Put('/:cardId/sequence')
+  @UseGuards(AuthGuard)
+  async UpdateCardSequence(
+    @Query('board_column_Id') board_column_Id: number,
+    @Param('cardId') cardId: number,
+    @Body() data: UpdateCardSequenceDto
+  ) {
+    await this.cardsService.UpdateCardSequence(board_column_Id, cardId, data.sequence);
   }
 }
--- src/cards/cards.service.ts
+++ src/cards/cards.service.ts
@@ -16,6 +16,9 @@ export class CardsService {
   async GetCards(board_column_Id: number) {
     const findCards = await this.cardRepository.find({ relations: ['board_column'] });
 
+    findCards.sort((a, b) => {
+      return a.sequence - b.sequence;
+    });
     return findCards.filter((card) => {
       return card.board_column.id == board_column_Id;
     });
@@ -45,14 +48,7 @@ export class CardsService {
   }
 
   //카드 수정
-  async UpdateCard(
-    board_column_Id: number,
-    id: number,
-    name: string,
-    content: string,
-    file_url: string,
-    sequence: number
-  ) {
+  async UpdateCard(board_column_Id: number, id: number, name: string, content: string, file_url: string) {
     const column = await this.boardColumnService.findOneBoardColumnById(board_column_Id); // BoardColumnService에서 컬럼 가져옴
     if (!column) {
       throw new NotFoundException('컬럼을 찾을 수 없습니다.');
@@ -62,11 +58,20 @@ export class CardsService {
       throw new NotFoundException('데이터 형식이 올바르지 않습니다.');
     }
 
-    await this.cardRepository.update(id, { name, content, file_url, sequence });
+    await this.cardRepository.update(id, { name, content, file_url });
   }
 
   //카드삭제
   async DeleteCard(board_column_Id: number, id: number) {
     await this.cardRepository.delete(id);
+  }
+
+  //카드 시퀀스 수정
+  async UpdateCardSequence(board_column_Id: number, cardId: number, sequence: number) {
+    const boardColumn = await this.boardColumnService.findOneBoardColumnById(board_column_Id);
+    const card = await this.cardRepository.findOneBy({ id: cardId });
+    if (!boardColumn) throw new NotFoundException('해당 보드는 존재하지 않습니다.');
+    if (!card) throw new NotFoundException('해당 칼럼은 존재하지 않습니다.');
+    await this.cardRepository.update({ id: cardId }, { sequence, board_column: boardColumn });
   }
 }
