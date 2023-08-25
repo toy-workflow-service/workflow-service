a5c13c860738d19eb77471472257d365bb393987
Sung min Park <somemen1234@naver.com>
2023-08-25 15:08:35 +0900

    chore: conflict resolved

--- src/views/assets/js/header.js
+++ src/views/assets/js/header.js
@@ -48,8 +48,8 @@ function deleteCookie(name) {
 }
 
 function setAccessToken() {
-  const result = getCookie('accessToken').split(' ')[1];
-  localStorage.setItem('accessToken', result);
+  const token = getCookie('accessToken');
+  localStorage.setItem('accessToken', token);
   deleteCookie('accessToken');
   window.location.reload();
 }
